91b2c4e5bfbaa46225302c29d4e28d728db069a4
Jakob Tidemand <111381232+jakotide@users.noreply.github.com>
2023-09-25 22:33:48 +0200

    Working on login user

--- js/components/register.js
+++ js/components/register.js
@@ -1,102 +1,28 @@
-  
-// const API_BASE_URL = "https://api.noroff.dev";
+//   const API_BASE_URL = "https://api.noroff.dev";
+// const regSuccess = document.querySelector(".reg-success");
 
-// async function registerUser(userData) {
-//   try {
-//     const registerURL = `${API_BASE_URL}/api/v1/social/auth/register`;
-
-//     const response = await fetch(registerURL, {
-//       method: "POST",
-//       body: JSON.stringify(userData),
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     });
-//     console.log(response);
-//     const json = await response.json();
-//     if (response.status >= 200 && response.status <= 299) {
-//       // Store the registered user data in local storage
-//       localStorage.setItem("registeredUserData", JSON.stringify(userData));
-//       console.log("Registration successful!");
-//     } else {
-//       console.log("Registration failed!");
-//     }
-//   } catch (error) {
-//     console.error(error);
-//   }
+// function validateName(name) {
+//   return /^[a-zA-Z0-9_]+$/.test(name);
 // }
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   const registrationForm = document.getElementById("registration-form");
+// function validateEmail(email) {
+//   const noroffEmail = /@noroff\.no$/;
+//   const studEmail = /@stud\.noroff\.no$/;
 
-//   registrationForm.addEventListener("submit", async (event) => {
-//     event.preventDefault();
-
-//     const name = document.getElementById("reg-name").value;
-//     const email = document.getElementById("reg-email").value;
-//     const password = document.getElementById("reg-password").value;
-
-//     const userData = {
-//       name,
-//       email,
-//       password,
-//     };
-
-//     console.log("userData:", userData);
-
-//     await registerUser(userData);
-//   });
-// });
-
-// // LOGIN
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const loginForm = document.getElementById("login-form");
-
-//   loginForm.addEventListener("submit", async (event) => {
-//     event.preventDefault();
-
-//     // Retrieve the registered user data from local storage
-//     const registeredUserData = JSON.parse(localStorage.getItem("registeredUserData"));
-
-//     if (registeredUserData) {
-//       // Use the registered user data to log in
-//       await loginUser(loginURL, registeredUserData);
-//     } else {
-//       console.log("No registered user data found.");
-//     }
-//   });
-// });
-
-// async function loginUser(url, userData) {
-//   try {
-//     const response = await fetch(url, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(userData),
-//     });
-
-//     const json = await response.json();
-//     console.log(json);
-
-//     if (response.status >= 200 && response.status <= 299) {
-//       console.log("Login successful!");
-
-//       const accessToken = json.accessToken;
-//       localStorage.setItem("accessToken", accessToken);
-//     } else {
-//       console.log("Login failed!");
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
+//   return noroffEmail.test(email) || studEmail.test(email);
 // }
 
-// const loginURL = `${API_BASE_URL}/api/v1/social/auth/login`;
+// function validatePassword(password) {
+//   return password.length >= 8;
+// }
 
-// const API_BASE_URL = "https://api.noroff.dev";
+// function displayError(fieldId, errorMessage) {
+//   const errorElement = document.getElementById(`${fieldId}-error`);
+//   if (errorElement) {
+//     errorElement.textContent = errorMessage;
+//     errorElement.style.display = errorMessage ? "block" : "none";
+//   }
+// }
 
 // async function registerUser(url, userData) {
 //   try {
@@ -106,42 +32,61 @@
 //         "Content-Type": "application/json"
 //       },
 //       body: JSON.stringify(userData),
-//     }
+//     };
+
 //     const response = await fetch(url, postData);
 //     const json = await response.json();
-//     console.log(json)
-//   } catch(error) {
-//     console.log(error)
+//     console.log(json);
+
+//         if (response.status >= 200 && response.status <= 299) {
+ 
+//           document.querySelector("#reg-name").value = "";
+//           document.querySelector("#reg-email").value = "";
+//           document.querySelector("#reg-password").value = "";
+          
+//           regSuccess.style.display = "block";
+//           console.log("Registration successful!");
+//         }
+//   } catch (error) {
+//     console.log(error.message);
 //   }
 // }
 
 // document.addEventListener("DOMContentLoaded", function () {
 //   const registrationForm = document.getElementById("registration-form");
-  
+
 //   registrationForm.addEventListener("submit", async (event) => {
 //     event.preventDefault();
 
-//     // Get the form field values inside the submit event handler
 //     const name = document.querySelector("#reg-name").value;
 //     const email = document.querySelector("#reg-email").value;
 //     const password = document.querySelector("#reg-password").value;
 
-//     const userToRegister = {
-//       name,
-//       email,
-//       password,
-//     };
+//     const isNameValid = validateName(name);
+//     const isEmailValid = validateEmail(email);
+//     const isPasswordValid = validatePassword(password);
 
-//     const registerURL = `${API_BASE_URL}/api/v1/social/auth/register`;
+//     displayError("name", isNameValid ? "" : "Username can not contain symbols apart from underscore");
+//     displayError("email", isEmailValid ? "" : "Email must be a valid stud.noroff.no or noroff.no email address");
+//     displayError("password", isPasswordValid ? "" : "Must be 8 characters long");
 
-//     try {
-//       await registerUser(registerURL, userToRegister);
-//     } catch (error) {
-//       console.log(error);
+//     if (isNameValid && isEmailValid && isPasswordValid) {
+//       const userToRegister = {
+//         name,
+//         email,
+//         password,
+//       };
+
+//       const registerURL = `${API_BASE_URL}/api/v1/social/auth/register`;
+
+//       try {
+//         await registerUser(registerURL, userToRegister);
+//       } catch (error) {
+//         console.log(error.message);
+//       }
 //     }
 //   });
 // });
-
 
 
 const API_BASE_URL = "https://api.noroff.dev";
@@ -184,20 +129,47 @@ async function registerUser(url, userData) {
     const json = await response.json();
     console.log(json);
 
-        // Check for a successful registration status (e.g., 200 or 201).
-        if (response.status === 200 || response.status === 201 || json.status === "success") {
-          // Clear input fields after successful registration.
-          document.querySelector("#reg-name").value = "";
-          document.querySelector("#reg-email").value = "";
-          document.querySelector("#reg-password").value = "";
-          
-          regSuccess.style.display = "block";
-    
-          // Optionally, you can display a success message here if needed.
-          console.log("Registration successful!");
-        }
+    if (response.status >= 200 && response.status <= 299) {
+
+      document.querySelector("#reg-name").value = "";
+      document.querySelector("#reg-email").value = "";
+      document.querySelector("#reg-password").value = "";
+
+      regSuccess.style.display = "block";
+      console.log("Registration successful!");
+    }
   } catch (error) {
     console.log(error.message);
+  }
+}
+
+async function loginUser(url, userData) {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userData),
+    });
+
+    const json = await response.json();
+    console.log(json);
+
+    if (response.status >= 200 && response.status <= 299) {
+      console.log("Login successful!");
+      const accessToken = json.accessToken;
+
+      // Store the access token in localStorage for future use
+      localStorage.setItem("accessToken", accessToken);
+
+      // Redirect to the authenticated user's dashboard or perform other actions as needed
+      window.location.href = "./feed/index.html";
+    } else {
+      console.log("Login failed!");
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
 
@@ -215,7 +187,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const isEmailValid = validateEmail(email);
     const isPasswordValid = validatePassword(password);
 
-    displayError("name", isNameValid ? "" : "Username can not contain symbols apart from underscore");
+    displayError("name", isNameValid ? "" : "Username cannot contain symbols apart from underscore");
     displayError("email", isEmailValid ? "" : "Email must be a valid stud.noroff.no or noroff.no email address");
     displayError("password", isPasswordValid ? "" : "Must be 8 characters long");
 
@@ -237,10 +209,31 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById("login-form");
+
+  loginForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector("#email-login").value;
+    const password = document.querySelector("#password-login").value;
+
+    const userToLogin = {
+      email,
+      password,
+    };
+
+    const loginURL = `${API_BASE_URL}/api/v1/social/auth/login`;
+
+    try {
+      await loginUser(loginURL, userToLogin);
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+});
 
 
-
-// LOGIN 
 
 
 
